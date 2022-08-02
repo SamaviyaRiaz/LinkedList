80dd2a6db92f4c45ade54440630d29b82c8c3df5
Kanwar Adnan <107979019+KanwarAdnan@users.noreply.github.com>
2022-08-02 19:53:00 +0500

    Update LinkedList2.cpp

--- LinkedList2.cpp
+++ LinkedList2.cpp
@@ -1,9 +1,10 @@
 using namespace std;
 #include<iostream>
 class Node{
-	public:
+private:
 	int data;
 	Node *next;
+public:
 	Node(){
 		int data=0;
 		next=NULL;
@@ -26,111 +27,112 @@ class Node{
 	}
 };
 class LinkedList{
-	public:
-		Node* head;
-		LinkedList(){
-			head=NULL;
-		}
-		LinkedList(Node* head){
-			setHead(head);
-		}
-		void setHead(Node* head){
-			this ->head=head;
-		}
-		Node* getHead(){
-			return head;
-		}
-		bool isEmpty(){
-			return (head->getNext()==NULL);
-		}
-		int getLength(){
-			int count=0;
-			if(!isEmpty()){
-				Node* temp=head;
-				while(temp!=NULL){
-					temp=temp->getNext();
-					count++;
-				}
-			}
-			return count;
-		}
-		void print(){
-			if(!isEmpty()){
-				Node* temp=head;
-				while(temp!=NULL){
-					cout<<temp->getData() << '\n';
-					temp = temp->getNext();
-				}
-			}
-		}
-		Node* getNthElement(int index=0){
-			if(! isEmpty()){
+private:
+	Node* head;
+public:
+	LinkedList(){
+		head=NULL;
+	}
+	LinkedList(Node* head){
+		setHead(head);
+	}
+	void setHead(Node* head){
+		this ->head=head;
+	}
+	Node* getHead(){
+		return head;
+	}
+	bool isEmpty(){
+		return (head->getNext()==NULL);
+	}
+	int getLength(){
+		int count=0;
+		if(!isEmpty()){
 			Node* temp=head;
-			for(int i=0;i<index-1;i++){
+			while(temp!=NULL){
 				temp=temp->getNext();
-			}
-			return temp;
-		}
-		return head;	
-		}
-		Node* getLastElement(){
-			return getNthElement(getLength()-1);
-		}
-		Node* getFirstElement(){
-			return head;
-		}
-		void addToBegining(int value){
-			if (!isEmpty())
-			{
-				Node* newNode = new Node(value);
-				newNode->setNext(head);
-				head = newNode;
+				count++;
 			}
 		}
-		void addAtLast(int value){
-			Node* newNode = new Node(value);
-			Node* temp = getLastElement();
-			temp->setNext(newNode);	
-		}
-		void dellLastElement(){
-			{
-				Node *temp1=head;
-				Node *temp2=temp1->getNext();
-				while(temp2!=NULL){
-					temp1=temp1->getNext();
-					temp2=temp2->getNext();
-				}
-				delete temp2;
-				temp1->setNext(NULL);
-			}
-			
-		}
-		void removeFromBegining(){
+		return count;
+	}
+	void print(){
+		if(!isEmpty()){
 			Node* temp=head;
-			head->getNext();
-			delete head;
-			head=temp;
+			while(temp!=NULL){
+				cout<<temp->getData() << '\n';
+				temp = temp->getNext();
+			}
 		}
-		void delNthElement(int index){
-			Node* temp1=head;
-			Node* temp2=temp1->getNext();
-			for(int i=0; i<index-2;i++){
+	}
+	Node* getNthElement(int index=0){
+		if(! isEmpty()){
+		Node* temp=head;
+		for(int i=0;i<index-1;i++){
+			temp=temp->getNext();
+		}
+		return temp;
+	}
+	return head;	
+	}
+	Node* getLastElement(){
+		return getNthElement(getLength()-1);
+	}
+	Node* getFirstElement(){
+		return head;
+	}
+	void addToBegining(int value){
+		if (!isEmpty())
+		{
+			Node* newNode = new Node(value);
+			newNode->setNext(head);
+			head = newNode;
+		}
+	}
+	void addAtLast(int value){
+		Node* newNode = new Node(value);
+		Node* temp = getLastElement();
+		temp->setNext(newNode);	
+	}
+	void dellLastElement(){
+		{
+			Node *temp1=head;
+			Node *temp2=temp1->getNext();
+			while(temp2!=NULL){
 				temp1=temp1->getNext();
 				temp2=temp2->getNext();
 			}
 			delete temp2;
-			temp1->getNext();
+			temp1->setNext(NULL);
 		}
-		void delElementOfValue(int value){
-			Node* temp=head;
-			while(temp!=NULL){
-				if(value==temp->getData()){
-					delete temp;
-					temp=temp->getNext();
-				}
+
+	}
+	void removeFromBegining(){
+		Node* temp=head;
+		temp = head->getNext();
+		delete head;
+		head=temp;
+	}
+	void delNthElement(int index){
+		Node* temp1=head;
+		Node* temp2=temp1->getNext();
+		for(int i=0; i<index-2;i++){
+			temp1=temp1->getNext();
+			temp2=temp2->getNext();
+		}
+		delete temp2;
+		temp1->getNext();
+	}
+	void delElementOfValue(int value){
+		Node* temp=head;
+		while(temp!=NULL){
+			if(value==temp->getData()){
+				delete temp;
+				temp=temp->getNext();
 			}
 		}
-		
+	}
+
 };
 int main(){
 	Node* head=new Node(2);
